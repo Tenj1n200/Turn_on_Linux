a424b9846f83e063296212af91d00eb8fed62778
Tenj1n200 <viceduartt@gmail.com>
2026-06-30 05:15:20 -0300

    Add main.py with Wake-on-LAN magic packet implementation

---
+++ main.py
@@ -0,0 +1,12 @@
+import socket
+
+mac = "00:e0:1a:b1:36:dc"
+
+mac = mac.replace(":", "")
+packet = bytes.fromhex("FF" * 6 + mac * 16)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+sock.sendto(packet, ("255.255.255.255", 9))
+
+print("Magic Packet enviado!")
